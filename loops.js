760b791a2b04be4e3710ab98dc31e6565fe811ce
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-14 14:06:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,6 +3,9 @@ function forLoop(array){
     if (i===1){
       array.push('I am 1 strange loop.')
     }
-    
+    else {
+      array.push('I am ${i} strange loops.')
+    }
   }
+  return array
 }
